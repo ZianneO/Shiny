13e23b50152f3cb93918121399d8e5327414c2fb
ZianneO <olverson.10@osu.edu>
2024-07-22 02:10:49 +0000

    Shiny App Launch

--- lm/app.r
+++ lm/app.r
@@ -1,151 +1,120 @@
-this is the code template with my additions in it #
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
-# Define UI for application that draws a histogram
+# Define UI
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-
-            
-            # Input: Select a file ----
-            fileInput("file1", "Choose CSV File",
-                      multiple = FALSE,
-                      accept = c("text/csv",
-                                 "text/comma-separated-values,text/plain",
-                                 ".csv")),
-            
-            # Horizontal line ----
-            tags$hr(),
-            
-            # Input: Checkbox if file has header ----
-            checkboxInput("header", "Header", TRUE),
-            
-            # Input: Select separator ----
-            radioButtons("sep", "Separator",
-                         choices = c(Comma = ",",
-                                     Semicolon = ";",
-                                     Tab = "\t"),
-                         selected = ","),
-            
-            # Input: Select quotes ----
-            radioButtons("quote", "Quote",
-                         choices = c(None = "",
-                                     "Double Quote" = '"',
-                                     "Single Quote" = "'"),
-                         selected = '"'),
-            
-            # Horizontal line ----
-            tags$hr(),
-            
-            # Input: Select number of rows to display ----
-            radioButtons("disp", "Display",
-                         choices = c(Head = "head",
-                                     All = "all"),
-                         selected = "head")
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot"),
-           plotOutput("lmPlot"),
-           tableOutput("contents")
-            verbatimTextOutput("Text")
-        )
+  
+  # Application title
+  titlePanel("Linear Regression Data"),
+  
+  # Sidebar with inputs
+  sidebarLayout(
+    sidebarPanel(
+      
+      # Input: Select a file
+      fileInput("file1", "Choose CSV File",
+                multiple = FALSE,
+                accept = c("text/csv",
+                           "text/comma-separated-values,text/plain",
+                           ".csv")),
+      
+      # Horizontal line
+      tags$hr(),
+      
+      # Input: Checkbox if file has header
+      checkboxInput("header", "Header", TRUE),
+      
+      # Input: Select separator
+      radioButtons("sep", "Separator",
+                   choices = c(Comma = ",",
+                               Semicolon = ";",
+                               Tab = "\t"),
+                   selected = ","),
+      
+      # Input: Select quotes
+      radioButtons("quote", "Quote",
+                   choices = c(None = "",
+                               "Double Quote" = '"',
+                               "Single Quote" = "'"),
+                   selected = '"'),
+      
+      # Horizontal line
+      tags$hr(),
+      
+      # Input: Select number of rows to display
+      radioButtons("disp", "Display",
+                   choices = c(Head = "head",
+                               All = "all"),
+                   selected = "head"),
+      
+      # Action button to trigger model update
+      actionButton("go", "Plot Linear Model")
+    ),
+    
+    # Show plots and table output
+    mainPanel(
+      plotOutput("distPlot"),
+      plotOutput("lmPlot"),
+      tableOutput("contents"),
+      verbatimTextOutput("values")
     )
+  )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    lmdata <- reactiveValues()
+# Define server logic
+server <- function(input, output, session) {
+  
+  lmdata <- reactiveValues()
+  
+  dataInput <- reactive({
+    req(input$file1)
     
-    dataInput <- reactive({
-        req(input$file1)
-        
-        df <- read.csv(input$file1$datapath,
-                       header = input$header,
-                       sep = input$sep,
-                       quote = input$quote)
-        return(df)
-    })
-
-    observeEvent(input$Push, (update_lm()})
-
-    })
-    
-    # output$distPlot <- renderPlot({
-    #     # generate bins based on input$bins from ui.R
-    #     x    <- faithful[, 2]
-    #     bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    #     print(bins)
-    #     # draw the histogram with the specified number of bins
-    #     hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    # })
-    # 
-    
-    output$distPlot <- renderPlot({
-        plot(dataInput()$x,dataInput()$y, xlab = "X", ylab = "Y")
-    })
-
-    
-
-    update_lm <- function(){
-        lmdata$model <- lm(y ~ x, data = dataInput())
-        lmdata$rsq <- summary(lmdata$model)$r.squared
-        lmdata$coef <- summary(lmdata$model)$coefficient
+    df <- read.csv(input$file1$datapath,
+                   header = input$header,
+                   sep = input$sep,
+                   quote = input$quote)
+    return(df)
+  })
+  
+  update_lm <- function() {
+    lmdata$model <- lm(y ~ x, data = dataInput())
+    lmdata$rsq <- summary(lmdata$model)$r.squared
+    lmdata$coef <- summary(lmdata$model)$coefficients[, 1]
+  }
+  
+  observeEvent(input$go, {
+    update_lm()
+  })
+  
+  output$distPlot <- renderPlot({
+    plot(dataInput()$x, dataInput()$y,
+         xlab = "X",
+         ylab = "Y",
+         main = "Distribution Plot",
+         col = "black",
+         border = "white")
+  })
+  
+  output$lmPlot <- renderPlot({
+    plot(dataInput()$x, dataInput()$y,
+         xlab = "X",
+         ylab = "Y",
+         main = "Linear Model Plot",
+         col = "black",
+         border = "white")
+    abline(lmdata$model, col = 'red')
+  })
+  
+  output$values <- renderText({
+    paste("R-squared = ", round(lmdata$rsq, 3))
+  })
+  
+  output$contents <- renderTable({
+    if (input$disp == "head") {
+      return(head(dataInput()))
+    } else {
+      return(dataInput())
     }
-
-    
-    output$lmPlot <- renderPlot({
-        plot(dataInput()$x,dataInput()$y, 
-             main = "Distribution Plot",
-             xlab = "X",
-             ylab = "Y",
-            col = "black",
-            border = "white")
-
-        output$lmPlot <- renderPlot({
-        plot(dataInput()$x,dataInput()$y, 
-             main = "Linear Model Plot",
-             xlab = "X",
-             ylab = "Y",
-            col = "black",
-            border = "white"
-            abline(lmdata$model, col = 'red')
-        
-    })
-
-    output$txt <- renderText({"R Squared Value =", lmdata$rsq, "Coefficients :", lmdata$coef})             
-                 
-    output$contents <- renderTable({
-        
-        # input$file1 will be NULL initially. After the user selects
-        # and uploads a file, head of that data file by default,
-        # or all rows if selected, will be shown.
-        
-        
-        if(input$disp == "head") {
-            return(head(dataInput()))
-        }
-        else {
-            return(dataInput())
-        }
-        
-    })
-        
+  })
 }
 
 # Run the application 
